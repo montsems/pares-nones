a56587b8767f2ff1f22d54b229fc702f8457fc13
montsems <montsems.web@gmail.com>
2017-09-28 14:56:40 +0200

    subida 1

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("no").onclick=function(){
+	document.getElementById("pantallaUno").style.display = "none";
+	document.getElementById("pantallaDosA").style.display = "block";
+	
+}
+
+document.getElementById("si").onclick=function(){
+	document.getElementById("pantallaUno").style.display = "none";
+	document.getElementById("pantallaDosB").style.display = "block";
+	
+}
+
+document.getElementById("si").onclick=function(){
+	document.getElementById("pantallaUno").style.display = "none";
+	document.getElementById("pantallaDosB").style.display = "block";
+	
+}
+
+document.getElementById("siOtraVez").onclick=function(){
+	document.getElementById("pantallaDosA").style.display = "none";
+	document.getElementById("pantallaDosB").style.display = "block";
+	
+}
